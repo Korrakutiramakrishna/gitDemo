b337bc4c23e1ed0fddc6232929e78f31d13bf683
ramakrishna <ramakrishnakorrakuti1122@gmail.com>
2026-07-09 14:37:31 +0530

    commit for broswers

---
+++ src/main/java/snippet/Snippet.java
@@ -0,0 +1,13 @@
+package snippet;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+
+public class Snippet {
+	public static void main(String[] args) 
+	{
+		WebDriver driver =new EdgeDriver();
+		
+	}
+}
+
