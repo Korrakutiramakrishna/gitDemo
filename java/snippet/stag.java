8f75f9a748970bef4fd7a583d8616001f0c14671
ramakrishna <ramakrishnakorrakuti1122@gmail.com>
2026-07-09 22:58:43 +0530

    sfmastra

--- java/snippet/stag.java
+++ java/snippet/stag.java
@@ -5,14 +5,14 @@ public class stag
 	public  static void Method()
 	{
 		System.out.println("org.openqa.selenium.SessionNotCreatedException ==>");
-<<<<<<< HEAD
+
 		System.out.println("org.openqa.selenium.SessionNotCreatedException :)stag.java");
 	}
 	public  static void Method(int number)
 	{
 		System.out.println("org.openqa.selenium.SessionNotCreatedException ==>");
 		System.out.println("org.openqa.selenium.SessionNotCreatedException :)stag.java");
-	}
+	}	
 	public  static void Method(char ch)
 	{
 		System.out.println("org.openqa.selenium.SessionNotCreatedException ==>");
