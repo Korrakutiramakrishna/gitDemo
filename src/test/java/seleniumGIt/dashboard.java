db79022bbff0a7228da3d6ad40bd1bd211eca16a
ramakrishna <ramakrishnakorrakuti1122@gmail.com>
2026-07-21 18:31:46 +0530

    last commit for  today

---
+++ src/test/java/seleniumGIt/dashboard.java
@@ -0,0 +1,15 @@
+package seleniumGIt;
+
+public class dashboard
+{
+	public static void main(String[] args) {
+		dashboard.syso();
+	}
+	public static void syso() 
+	{
+		System.out.println("jelllskds");
+		// TODO Auto-generated method stub
+
+	}
+
+}
