23d8424fc1b25858b511f3f26846dc1e7545836f
ramakrishna <ramakrishnakorrakuti1122@gmail.com>
2026-07-21 12:04:20 +0530

    today commti in GitDemo 07/21/2026

---
+++ src/test/java/seleniumGIt/ksds.java
@@ -0,0 +1,21 @@
+package seleniumGIt;
+
+public class ksds 
+{
+	public static void main(String[] args)
+	{
+		System.out.println("2");
+		
+	}
+	public static void importdata()
+	{
+		System.out.println("2");
+		
+	}
+
+	public static void importdata1()
+	{System.out.println("2");
+	
+	}
+		
+}
