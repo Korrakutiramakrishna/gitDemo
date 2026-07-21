73bfbb66bfdb7f6240a2654e415494547d30f4fd
ramakrishna <ramakrishnakorrakuti1122@gmail.com>
2026-07-21 17:56:22 +0530

    lakshmi

--- src/test/java/seleniumGIt/demoss.java
+++ src/test/java/seleniumGIt/demoss.java
@@ -15,5 +15,9 @@ public class demoss
 	public static void main(String[] args) {
 		System.out.println("With commiteibg the code nad  pull in antoher branch");
 	}
+	
+	public static void lakshmi(String[] args) {
+		System.out.println("With commiteibg the code nad  pull in antoher branch");
+	}
 
 }
