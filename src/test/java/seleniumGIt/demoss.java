9642e62864fce3013994c88154d29ea8cc62068d
ramakrishna <ramakrishnakorrakuti1122@gmail.com>
2026-07-21 12:33:04 +0530

    sdfsdfosdfjs

--- src/test/java/seleniumGIt/demoss.java
+++ src/test/java/seleniumGIt/demoss.java
@@ -2,6 +2,14 @@ package seleniumGIt;
 
 public class demoss
 {
+	public static void methods(String[] args) {
+		System.out.println("With commiteibg the code nad  pull in antoher branch");
+	}
+	public static void metjh(String[] args) {
+		System.out.println("With commiteibg the code nad  pull in antoher branch");
+	}
+	
+	
 	public static void main(String[] args) {
 		System.out.println("With commiteibg the code nad  pull in antoher branch");
 	}
