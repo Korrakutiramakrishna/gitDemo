205a27a490e1e9d99864f0c541577c2525fb3bc3
ramakrishna <ramakrishnakorrakuti1122@gmail.com>
2026-07-21 18:03:38 +0530

    collections status

---
+++ src/test/java/seleniumGIt/collections.java
@@ -0,0 +1,24 @@
+package seleniumGIt;
+
+import java.util.ListIterator;
+import java.util.Vector;
+
+public class collections
+{
+	public static void main(String[] args) {
+     Vector v1 =new Vector();
+     System.out.println(v1.capacity());
+     for(char ch='A';ch<='Z';ch++)
+     {
+    	 v1.add(ch);
+     }
+     ListIterator i1=v1.listIterator();
+     System.out.println(v1.capacity());
+     while(i1.hasNext())
+     {
+     System.out.println(i1.next());
+     System.out.println(i1.hasPrevious());
+     }
+        /* it used for read the */
+    }
+}
