35feb245ccc39e144e0fb190c8e8c639a4d0b2b0
ramakrishna <ramakrishnakorrakuti1122@gmail.com>
2026-07-21 11:46:23 +0530

    todays commited files 7/21/2026

---
+++ java/snippet/develops.java
@@ -0,0 +1,11 @@
+package snippet;
+
+public class develops 
+{
+	public static void main(String[]args)
+	{
+		Scanner sn=new Scanner(System.in);
+		System.out.println("enter the numbers");
+	}
+	
+}
