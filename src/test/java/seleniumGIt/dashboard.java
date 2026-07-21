67fef5e06a712c3fdc9b42ce15665a72e43e9ef4
ramakrishna <ramakrishnakorrakuti1122@gmail.com>
2026-07-21 18:38:46 +0530

    new commutfor dashoard lasted commit

--- src/test/java/seleniumGIt/dashboard.java
+++ src/test/java/seleniumGIt/dashboard.java
@@ -4,8 +4,15 @@ public class dashboard
 {
 	public static void main(String[] args) {
 		dashboard.syso();
+		dashboard.sysoo();
 	}
 	public static void syso() 
+	{
+		System.out.println("jelllskds");
+		// TODO Auto-generated method stub
+
+	}
+	public static void sysoo() 
 	{
 		System.out.println("jelllskds");
 		// TODO Auto-generated method stub
