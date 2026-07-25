5bf57d143084da1178950eee7254d6feeb3972e5
ramakrishna <ramakrishnakorrakuti1122@gmail.com>
2026-07-25 15:48:33 +0530

    ssdioefjeje

--- src/test/java/seleniumGIt/dashboard.java
+++ src/test/java/seleniumGIt/dashboard.java
@@ -2,21 +2,36 @@ package seleniumGIt;
 
 public class dashboard
 {
-	public static void main(String[] args) {
-		dashboard.syso();
-		dashboard.sysoo();
-	}
-	public static void syso() 
-	{
-		System.out.println("jelllskds");
-		// TODO Auto-generated method stub
+//	public static void main(String[] args) {
+//		dashboard.syso();
+//		dashboard.sysoo();
+//	}
+//	public static void syso() 
+//	{
+//		System.out.println("jelllskds");
+//		// TODO Auto-generated method stub
+//
+//	}
+//	public static void sysoo() 
+//	{
+//		System.out.println("jelllskds");
+//		// TODO Auto-generated method stub
+//
+//	}
+	
+	    
+	        public static void main(String args[]) 
+	        {
+	            try 
+	            {
+	                System.out.print("A");
+	                throw new NullPointerException ("Hello");
+	            }
+	            catch(ArithmeticException e) 
+	            {
+	                System.out.print("B");        	
+	            }
+	        }
+	    }
 
-	}
-	public static void sysoo() 
-	{
-		System.out.println("jelllskds");
-		// TODO Auto-generated method stub
 
-	}
-
-}
