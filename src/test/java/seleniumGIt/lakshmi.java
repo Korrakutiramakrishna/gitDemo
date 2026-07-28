5fac5df12926f78308f147e7b9d58101de87904e
ramakrishna <ramakrishnakorrakuti1122@gmail.com>
2026-07-29 00:04:57 +0530

    shifting branches:

--- src/test/java/seleniumGIt/lakshmi.java
+++ src/test/java/seleniumGIt/lakshmi.java
@@ -1,16 +1,20 @@
 package seleniumGIt;
 
 public class lakshmi {
-	
-	public static void main(String[] args) {
-		
-	
-	
-	int a =10;
-	int b=20;
-	int c=a+b;
-	
-	System.out.println("a+b=>"+c);
 
-}
+	public static void main(String[] args) {
+		int a =10;
+		int b=20;
+		System.out.println(a);
+		System.out.println(b);
+		a=a+b;
+		b=a-b;
+		a=a-b;
+		System.out.println(a);
+		System.out.println(b);
+		int c=a+b;
+		System.out.println("a+b=>"+c);
+		System.out.println("afsdfsdf");
+
+	}
 }
