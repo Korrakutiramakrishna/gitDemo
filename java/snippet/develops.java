85aa69c392c44ecee05e2aeb3f44b27eed92e37f
ramakrishna <ramakrishnakorrakuti1122@gmail.com>
2026-07-29 17:56:46 +0530

    todayscomits

--- java/snippet/develops.java
+++ java/snippet/develops.java
@@ -1,11 +1,16 @@
 package snippet;
 
+import java.util.Scanner;
+
 public class develops 
 {
 	public static void main(String[]args)
 	{
 		Scanner sn=new Scanner(System.in);
 		System.out.println("enter the numbers");
+		int n=sn.nextInt()
+		System.out.println("helloword==>");
+		String s1="sfsgsdgsd";		
 	}
 	
 }
